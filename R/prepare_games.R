79d4c5e6716566195b7245c48c9b158761ead973
Aaron Peikert <aaron.peikert@posteo.de>
2022-08-08 14:45:12 +0200

    i.e.

--- R/prepare_games.R
+++ R/prepare_games.R
@@ -1,12 +1,12 @@
 # 1. list requirements early
-# (load libraries at the beginning)
+# (i.e., load libraries at the beginning)
 library(here)
 library(tidyverse)
 library(lubridate)
 
 #----load-inflation----
 # 2. use relative locations
-# (relative paths instead absolute, names instead of indices)
+# (i.e., relative paths instead absolute, names instead of indices)
 games_raw <- readr::read_csv(
   # directly download and load data
   "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-03-16/games.csv"
@@ -14,6 +14,6 @@ games_raw <- readr::read_csv(
 
 #----transform-inflation----
 # 3. document relevant information
-# (variable names are documentation + comments)
+# (i.e., variable names are documentation + comments)
 games <- mutate(date = str_c(month, "_", year) %>% # concatenate month and year
                   my()) # my = month year -> convert to date
