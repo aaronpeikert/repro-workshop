d125f5b5f57e28e67bf06e9d1b7c23d60a79ff34
Hannes Diemerling <hannes.diemerling@gmail.com>
2023-09-05 12:56:46 +0200

    CamelCase to snake_case for download_inflation.R

--- R/download_inflation.R
+++ R/download_inflation.R
@@ -6,7 +6,7 @@ inflation_raw_path <- here("data", "raw", "inflation.xlsx")
 fs::dir_create(fs::path_dir(inflation_raw_path))
 
 # Function to attempt Google Drive download
-downloadFromGoogle <- function() {
+download_from_google <- function() {
   download.file(
     "https://drive.google.com/uc?export=download&id=1czE3r1UEF21XvRGkXna0SE7yVvvrSiG3",
     fs::path_ext_set(inflation_raw_path, "rds")
@@ -14,7 +14,7 @@ downloadFromGoogle <- function() {
 }
 
 # Function to attempt Bank of England download and then process it
-downloadFromBankOfEngland <- function() {
+download_from_bank_of_england <- function() {
   system(paste("curl -o", inflation_raw_path, "https://www.bankofengland.co.uk/-/media/boe/files/inflation-attitudes-survey/individual-responses-xlsx.xlsx"))
   inflation_raw <- readxl::read_xlsx(here("data", "raw", "inflation.xlsx"), sheet = 1)
   readr::write_rds(inflation_raw, fs::path_ext_set(inflation_raw_path, "rds"), compress = "gz")
@@ -22,7 +22,7 @@ downloadFromBankOfEngland <- function() {
 
 # Use tryCatch to first attempt Google Drive download and if that fails, attempt Bank of England download
 tryCatch({
-  downloadFromGoogle()
+  download_from_google()
 }, error = function(e) {
-  downloadFromBankOfEngland()
+  download_from_bank_of_england()
 })
