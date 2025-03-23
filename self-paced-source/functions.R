5eb2620c16dc3ce1b640e72387513ea68fd086b5
Hannes Diemerling <53815471+HannesDiemerling@users.noreply.github.com>
2025-03-23 16:02:58 +0100

    Update functions.R

--- self-paced-source/functions.R
+++ self-paced-source/functions.R
@@ -32,7 +32,7 @@ generate_reprodudes <- function(links, title, text) {
 }
 
 
-generate_fullwidthinfo <- function(links, title, text) {
+generate_additionalinfo_full <- function(links, title, text) {
   template <- '
 <div class="infobox-full" onclick="window.open(\'{{links}}\', \'_blank\')">
   <h2 class="infobox-title">{{title}}</h2>
@@ -46,3 +46,4 @@ generate_fullwidthinfo <- function(links, title, text) {
   
   return(html)
 }
+
