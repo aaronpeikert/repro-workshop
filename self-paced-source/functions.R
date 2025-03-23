7d45e33db12ed36ecf1610f77c4646e3dacb81fa
Hannes Diemerling <53815471+HannesDiemerling@users.noreply.github.com>
2025-03-23 15:56:31 +0100

    Update functions.R

--- self-paced-source/functions.R
+++ self-paced-source/functions.R
@@ -30,3 +30,19 @@ generate_reprodudes <- function(links, title, text) {
   
   return(html)
 }
+
+
+generate_fullwidthinfo <- function(links, title, text) {
+  template <- '
+<div class="infobox-full" onclick="window.open(\'{{links}}\', \'_blank\')">
+  <h2 class="infobox-title">{{title}}</h2>
+  <p class="infobox-content">{{text}}</p>
+</div>
+  '
+  
+  values <- list(links = links, title = title, text = text)
+  
+  html <- whisker.render(template, values)
+  
+  return(html)
+}
